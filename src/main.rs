733e48aec1f221ea9bddc08fcda1a83a1c071f31
Tommi Kabelitz <tommi.kabelitz@priorianalytica.com>
2025-08-14 18:26:29 +0930

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,39 @@
+use std::io;
 use std::mem::size_of;
 use std::thread::sleep;
 use std::time::Duration;
-use std::{io, time};
 
 mod perf_events;
 
 use libc::{close, ioctl, read};
 use perf_events::{
-    PERF_COUNT_HW_BRANCH_INSTRUCTIONS, PERF_COUNT_HW_BRANCH_MISSES, PERF_COUNT_HW_CACHE_MISSES,
-    PERF_COUNT_HW_CACHE_REFERENCES, PERF_EVENT_IOC_DISABLE, PERF_EVENT_IOC_ENABLE,
-    PERF_EVENT_IOC_RESET, PERF_TYPE_HARDWARE, PerfEventAttr, flags::PerfEventFlags,
-    perf_event_open,
+    PERF_EVENT_IOC_DISABLE, PERF_EVENT_IOC_ENABLE, PERF_EVENT_IOC_RESET, PerfEventAttr,
+    flags::PerfEventFlags, perf_event_open,
 };
+
+use crate::perf_events::EventType;
 
 fn main() -> io::Result<()> {
     let cpu_id = 0;
 
-    let mut cache_ref_attrs = PerfEventAttr::default();
-    cache_ref_attrs.type_ = PERF_TYPE_HARDWARE;
-    cache_ref_attrs.size = size_of::<PerfEventAttr>() as u32;
-    cache_ref_attrs.config = PERF_COUNT_HW_CACHE_REFERENCES;
-    cache_ref_attrs.flags = (PerfEventFlags::DISABLED | PerfEventFlags::EXCLUDE_KERNEL).bits();
+    let mut cache_ref_attrs = PerfEventAttr::new(EventType::CacheReferences)
+        .with_flags(PerfEventFlags::DISABLED | PerfEventFlags::EXCLUDE_KERNEL);
 
     let cache_ref_fd = perf_event_open(&mut cache_ref_attrs, -1, cpu_id, -1, 0)?;
     let parent_fd = cache_ref_fd;
 
-    let mut cache_miss_attrs = PerfEventAttr::default();
-    cache_miss_attrs.type_ = PERF_TYPE_HARDWARE;
-    cache_miss_attrs.size = size_of::<PerfEventAttr>() as u32;
-    cache_miss_attrs.config = PERF_COUNT_HW_CACHE_MISSES;
-    cache_miss_attrs.flags = PerfEventFlags::EXCLUDE_KERNEL.bits();
+    let mut cache_miss_attrs = PerfEventAttr::new(EventType::CacheMisses)
+        .with_flags(PerfEventFlags::DISABLED | PerfEventFlags::EXCLUDE_KERNEL);
 
     let cache_miss_fd = perf_event_open(&mut cache_miss_attrs, -1, cpu_id, parent_fd, 0)?;
 
-    let mut branch_retired_attrs = PerfEventAttr::default();
-    branch_retired_attrs.type_ = PERF_TYPE_HARDWARE;
-    branch_retired_attrs.size = size_of::<PerfEventAttr>() as u32;
-    branch_retired_attrs.config = PERF_COUNT_HW_BRANCH_INSTRUCTIONS;
-    branch_retired_attrs.flags = PerfEventFlags::EXCLUDE_KERNEL.bits();
+    let mut branch_retired_attrs = PerfEventAttr::new(EventType::BranchInstructions)
+        .with_flags(PerfEventFlags::DISABLED | PerfEventFlags::EXCLUDE_KERNEL);
 
     let branch_retired_fd = perf_event_open(&mut branch_retired_attrs, -1, cpu_id, parent_fd, 0)?;
 
-    let mut branch_miss_attrs = PerfEventAttr::default();
-    branch_miss_attrs.type_ = PERF_TYPE_HARDWARE;
-    branch_miss_attrs.size = size_of::<PerfEventAttr>() as u32;
-    branch_miss_attrs.config = PERF_COUNT_HW_BRANCH_MISSES;
-    branch_miss_attrs.flags = PerfEventFlags::EXCLUDE_KERNEL.bits();
+    let mut branch_miss_attrs = PerfEventAttr::new(EventType::BranchMisses)
+        .with_flags(PerfEventFlags::DISABLED | PerfEventFlags::EXCLUDE_KERNEL);
 
     let branch_miss_fd = perf_event_open(&mut branch_miss_attrs, -1, cpu_id, parent_fd, 0)?;
 
--- src/perf_events/mod.rs
+++ src/perf_events/mod.rs
@@ -3,12 +3,28 @@ use std::os::unix::io::RawFd;
 
 use libc::{_IO, Ioctl, SYS_perf_event_open, pid_t, syscall};
 
+use crate::perf_events::flags::PerfEventFlags;
+
 pub mod flags;
 
 pub const PERF_EVENT_IOC_ENABLE: Ioctl = _IO(b'$' as u32, 0);
 pub const PERF_EVENT_IOC_DISABLE: Ioctl = _IO(b'$' as u32, 1);
 pub const _PERF_EVENT_IOC_REFRESH: Ioctl = _IO(b'$' as u32, 2);
 pub const PERF_EVENT_IOC_RESET: Ioctl = _IO(b'$' as u32, 3);
+
+#[allow(dead_code)]
+pub enum EventType {
+    CpuCycles = 0,
+    Instructions = 1,
+    CacheReferences = 2,
+    CacheMisses = 3,
+    BranchInstructions = 4,
+    BranchMisses = 5,
+    BusCycles = 6,
+    StalledCyclesFrontend = 7,
+    StalledCyclesBackend = 8,
+    RefCpuCycles = 9,
+}
 
 #[repr(C)]
 #[derive(Debug, Default)]
@@ -34,18 +50,51 @@ pub struct PerfEventAttr {
     pub __reserved_2: u16,
 }
 
+impl PerfEventAttr {
+    pub fn new(event: EventType) -> Self {
+        Self {
+            type_: PERF_TYPE_HARDWARE,
+            size: size_of::<Self>() as u32,
+            config: event as u64,
+            sample_period_or_freq: 0,
+            sample_type: 0,
+            read_format: 0,
+            flags: 0,
+            wakeup_events_or_watermark: 0,
+            bp_type: 0,
+            config1: 0,
+            config2: 0,
+            branch_sample_type: 0,
+            sample_regs_user: 0,
+            sample_stack_user: 0,
+            clockid: 0,
+            sample_regs_intr: 0,
+            aux_watermark: 0,
+            sample_max_stack: 0,
+            __reserved_2: 0,
+        }
+    }
+
+    pub fn with_flags(self, flags: PerfEventFlags) -> Self {
+        Self {
+            flags: flags.bits(),
+            ..self
+        }
+    }
+}
+
 pub const PERF_TYPE_HARDWARE: u32 = 0;
 
-pub const PERF_COUNT_HW_CPU_CYCLES: u64 = 0;
-pub const PERF_COUNT_HW_INSTRUCTIONS: u64 = 1;
-pub const PERF_COUNT_HW_CACHE_REFERENCES: u64 = 2;
-pub const PERF_COUNT_HW_CACHE_MISSES: u64 = 3;
-pub const PERF_COUNT_HW_BRANCH_INSTRUCTIONS: u64 = 4;
-pub const PERF_COUNT_HW_BRANCH_MISSES: u64 = 5;
-pub const PERF_COUNT_HW_BUS_CYCLES: u64 = 6;
-pub const PERF_COUNT_HW_STALLED_CYCLES_FRONTEND: u64 = 7;
-pub const PERF_COUNT_HW_STALLED_CYCLES_BACKEND: u64 = 8;
-pub const PERF_COUNT_HW_REF_CPU_CYCLES: u64 = 9;
+// pub const _PERF_COUNT_HW_CPU_CYCLES: u64 = 0;
+// pub const _PERF_COUNT_HW_INSTRUCTIONS: u64 = 1;
+// pub const PERF_COUNT_HW_CACHE_REFERENCES: u64 = 2;
+// pub const PERF_COUNT_HW_CACHE_MISSES: u64 = 3;
+// pub const PERF_COUNT_HW_BRANCH_INSTRUCTIONS: u64 = 4;
+// pub const PERF_COUNT_HW_BRANCH_MISSES: u64 = 5;
+// pub const _PERF_COUNT_HW_BUS_CYCLES: u64 = 6;
+// pub const _PERF_COUNT_HW_STALLED_CYCLES_FRONTEND: u64 = 7;
+// pub const _PERF_COUNT_HW_STALLED_CYCLES_BACKEND: u64 = 8;
+// pub const _PERF_COUNT_HW_REF_CPU_CYCLES: u64 = 9;
 
 pub fn perf_event_open(
     attr: &mut PerfEventAttr,
